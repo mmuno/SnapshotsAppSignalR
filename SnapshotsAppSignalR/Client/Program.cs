251a4d4e94f078cf7b8a3cd5823dc58f66a27f76
maomun <mauricio.munoz@skf.com>
2020-04-17 17:36:56 -0600

    Replaced chart library with Blazorise

--- SnapshotsAppSignalR/Client/Program.cs
+++ SnapshotsAppSignalR/Client/Program.cs
@@ -1,7 +1,7 @@
-using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,11 +12,26 @@ namespace SnapshotsAppSignalR.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+
+            builder.Services
+                .AddBlazorise(options =>
+                {
+                    options.ChangeTextOnKeyPress = true;
+                })
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons();
 
             builder.Services.AddBaseAddressHttpClient();
 
-            await builder.Build().RunAsync();
+            builder.RootComponents.Add<App>("app");
+
+            var host = builder.Build();
+
+            host.Services
+                .UseBootstrapProviders()
+                .UseFontAwesomeIcons();
+
+            await host.RunAsync();
         }
     }
 }
