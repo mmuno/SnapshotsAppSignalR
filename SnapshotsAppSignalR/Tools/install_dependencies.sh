c88f66f35c6b38321668a152fdccfed85ecc275d
mauricio <mauricio.munoz@skf.com>
2020-04-20 23:37:59 +0000

    Ficed docker file and script

--- SnapshotsAppSignalR/Tools/install_dependencies.sh
+++ SnapshotsAppSignalR/Tools/install_dependencies.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
-sudo apt update
-sudo apt upgrade
-sudo apt install pkg-config autoconf autogen python3-pip
+apt update
+apt --assume-yes install pkg-config autoconf autogen python3-pip git libtool
 git clone https://github.com/zeromq/libzmq
 cd libzmq
 ./autogen.sh && ./configure && make -j 4
-make check && sudo make install && sudo ldconfig
+make check && make install && ldconfig
 cd .. && rm -rf libzmq
 
-git clone git@github.com:zeromq/cppzmq.git
+git clone https://github.com/zeromq/cppzmq.git
 cd cppzmq
 mkdir build
 cd build
 cmake ..
-sudo make -j4 install
+make -j4 install
 cd ../../
 rm -rf cppzmq
 
-git clone git@github.com:jbeder/yaml-cpp.git
+git clone https://github.com/jbeder/yaml-cpp.git
 cd yaml-cpp
 mkdir build
 cd build
 cmake ..
-sudo make -j4 install
+make -j4 install
 cd ../../
-sudo rm -rf yaml-cpp
+rm -rf yaml-cpp
 
-git clone git@github.com:facebook/zstd.git
+git clone https://github.com/facebook/zstd.git
 cd zstd
-sudo make -j4 install
+make -j4 install
 cd ..
-sudo rm -rf zstd
+rm -rf zstd
 
 pip3 install -r requirements.txt
