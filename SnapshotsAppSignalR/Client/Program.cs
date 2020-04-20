88cc35859c00d10404246c39582542a189201414
maomun <mauricio.munoz@skf.com>
2020-04-20 17:43:27 -0600

    Removed Blazorise

--- SnapshotsAppSignalR/Client/Program.cs
+++ SnapshotsAppSignalR/Client/Program.cs
@@ -1,7 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using Blazorise;
-using Blazorise.Bootstrap;
-using Blazorise.Icons.FontAwesome;
+using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,26 +12,11 @@ namespace SnapshotsAppSignalR.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-            builder.Services
-                .AddBlazorise(options =>
-                {
-                    options.ChangeTextOnKeyPress = true;
-                })
-                .AddBootstrapProviders()
-                .AddFontAwesomeIcons();
+            builder.RootComponents.Add<App>("app");
 
             builder.Services.AddBaseAddressHttpClient();
 
-            builder.RootComponents.Add<App>("app");
-
-            var host = builder.Build();
-
-            host.Services
-                .UseBootstrapProviders()
-                .UseFontAwesomeIcons();
-
-            await host.RunAsync();
+            await builder.Build().RunAsync();
         }
     }
 }
